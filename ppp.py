2304f56bd4ba5e02f1885f22275629432b51a34b
sarawalk <f.sarawalk@gmail.com>
2024-05-15 16:19:03 +0200

    pypdf read them!

--- ppp.py
+++ ppp.py
@@ -7,16 +7,14 @@ def ppp(file, page=None):
     if reader.is_encrypted:
         reader.decrypt(password='')
         
-    n_pages=reader.get_num_pages()
-    # all pages
-    pages=reader.flattened_pages
+    n_pages=len(reader.pages)
     
     if page is not None:
         assert type(page)==int and page<n_pages
-        text=pages[page].extract_text()
+        text=reader.pages[page].extract_text()
     else:
         # make all text in the same page
-        pages=[p.extract_text() for p in pages]
+        pages=[reader.pages[p].extract_text() for p in range(n_pages)]
         text=' '.join(pages)
     # delete new line
     text=' '.join(text.split('\n'))
